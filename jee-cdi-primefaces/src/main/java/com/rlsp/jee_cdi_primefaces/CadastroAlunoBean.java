88aef43f85d712f810fb0f362d542db4290da249
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-03-20 18:01:18 -0300

    2.1

---
+++ jee-cdi-primefaces/src/main/java/com/rlsp/jee_cdi_primefaces/CadastroAlunoBean.java
@@ -0,0 +1,39 @@
+package com.rlsp.jee_cdi_primefaces;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class CadastroAlunoBean implements Serializable{
+
+	private static final long serialVersionUID = 6437555139259136240L;
+
+	private String nome;
+	private String email;
+	
+	public void cadastrarAluno() {
+		System.out.println("Nome do Aluno : " + this.nome);
+		System.out.println("E-mail do Aluno : " + this.email);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
